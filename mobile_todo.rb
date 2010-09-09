ec4f0929657e10a4a44da459aa8a6f1a1c386d04
Jacky Chan <linux_china@Jacky-Chans-MacBook-Pro.local>
2010-09-09 15:28:44 +0800

    add html filter feature

--- mobile_todo.rb
+++ mobile_todo.rb
@@ -1,9 +1,27 @@
 require 'rubygems'
 require 'sinatra'
 require 'rio'
+require 'open-uri'
+require 'hpricot'
 
 configure :production do
   # configure stuff for production
+end
+
+# filter html
+def filter_html(base_url, page_content)
+  doc = Hpricot(page_content)
+  (doc/'//a').each do |link|
+    href = "#{link.attributes['href']}"
+    href = "#{base_uri}#{href}" if href.start_with?("/")
+    link.attributes['href'] = "/proxy/#{href}" if (href.start_with?("http"))
+  end
+  (doc/'//img').each do |img|
+    src = "#{img.attributes['src']}"
+    src = "#{base_uri}#{src}" if src.start_with?("/")
+    img.attributes['src'] = "/proxy/#{src}" if src.start_with?("http")
+  end
+  doc.to_s
 end
 
 get '/' do
@@ -11,10 +29,23 @@ get '/' do
 end
 
 get '/proxy/*' do
-
-  url = "#{params[:splat][0]}"
-  url = "http://#{url}" if (!url.start_with?("http"))
-  rio(url).contents
+  dest_url = "#{params[:splat][0]}"
+  dest_url = "http://#{dest_url}" if (!dest_url.start_with?("http"))
+  uri = URI.parse(dest_url)
+  base_uri = "http://#{uri.host}:#{uri.port}"
+  content_type = nil
+  charset = nil
+  page_content = nil
+  open(uri, "User-Agent" => "Ruby/#{RUBY_VERSION}") do |f|
+    content_type =  f.content_type
+    charset  = f.charset
+    page_content = f.read
+  end
+  content_type content_type, :charset => charset
+  if (content_type.include?('html') || content_type.include?('text'))
+    page_content = filter_html(base_uri, page_content)
+  end
+  page_content
 end
 
 # app specific information. only for test
